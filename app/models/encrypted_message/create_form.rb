f76aea1f247952d87fa85e4dbba371d4b5934379
Kacper Walanus <kacper@walanus.com>
2016-12-23 22:14:55 +0100

    content can’t have less than 6 chars

--- app/models/encrypted_message/create_form.rb
+++ app/models/encrypted_message/create_form.rb
@@ -2,6 +2,6 @@ class EncryptedMessage::CreateForm
   include ActiveModel::Model
 
   attr_accessor :decrypted_content, :password, :question
-  validates :decrypted_content, presence: { message: "Message can't be blank" }
+  validates :decrypted_content, presence: { message: "Message can't be blank" }, length: { minimum: 6 }
   validates :password, presence: true, length: { minimum: 6 }
 end
